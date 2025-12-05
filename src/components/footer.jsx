61306dadfaee53de75bd475c70e36065910ae23e
iarocci44-byte <iarocci44@gmail.com>
2025-12-05 17:45:21 -0500

    fixed the underline hover option under my name

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <div className='footer'>
         <p>© 2025 Movie Explorer | Powered by OMDb API</p>
-        <p className='nav__links2'>Created by <Link to="http://christopheriarocci.me/" target="_blank" rel="noopener noreferrer"><span className="yellow__link--bg">Christopher Iarocci</span></Link></p>
+        <p className='nav__links2'>Created by <Link to="http://christopheriarocci.me/" className='nav__links2' target="_blank" rel="noopener noreferrer"><span className="yellow__link--bg">Christopher Iarocci</span></Link></p>
     </div>
   )
 }
